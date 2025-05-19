818b0ede43c50070bf588974f63ee0012f72e332
ROHIT3496484 <rohitrana349@gmail.com>
2025-05-19 17:01:06 +0530

    Final

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Infinitescroll from "./mccomponents/Infinite Scroll/Infinitescroll";
+
+function App() {
+  return (
+    <div className="App">
+      <Infinitescroll/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mccomponents/Infinite Scroll/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+    </div>
+  )
+}
+
+export default Post
